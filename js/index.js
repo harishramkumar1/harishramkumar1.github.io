305412fefc7c1e6df20f0ce9685e960ed00eff2c
Bal subramanian <Harish.Balsubramanian@BNSF.com>
2017-05-09 00:17:01 -0500

    changing to portfolio page

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$('.about').on('click', function() {
+    $('html, body').animate({
+                    scrollTop: $("#abtdiv").offset().top
+                }, 2000);
+    
+    return false;
+});
+
+$("#contactdiv").on('click', function() {
+    $('html, body').animate({
+                    scrollTop: $("#contactDiv").offset().top
+                }, 2000);
+    
+    return false;
+});
+
+$('#homediv').on('click', function() {
+    $('html, body').animate({
+                    scrollTop: $("#hmdiv").offset().top
+                }, 2000);
+    
+    return false;
+});
+ $(document).ready(function() {
+   
+ });
